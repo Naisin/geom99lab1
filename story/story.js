b58e208a12a746fd3bcce86c6eac9ce1d8c1272d
Naisin <146375897+Naisin@users.noreply.github.com>
2024-01-24 16:11:49 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -3,7 +3,7 @@ function initMap() {
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 6,
-      center: { lat: 41.85, lng: -87.65 },
+      center: { lat: 32.7266, lng: 74.8570 },
     });
   
     directionsRenderer.setMap(map);
