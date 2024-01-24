fd2980c93704ba3846a40f0e6a0b7b86914241a0
Naisin <146375897+Naisin@users.noreply.github.com>
2024-01-24 14:21:21 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,25 +1,36 @@
-function initMap() {
+function initMap(): void {
   const directionsRenderer = new google.maps.DirectionsRenderer();
   const directionsService = new google.maps.DirectionsService();
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
-    center: { lat: 37.77, lng: -122.447 },
-  });
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 14,
+      center: { lat: 37.77, lng: -122.447 },
+    }
+  );
 
   directionsRenderer.setMap(map);
+
   calculateAndDisplayRoute(directionsService, directionsRenderer);
-  document.getElementById("mode").addEventListener("change", () => {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-  });
+  (document.getElementById("mode") as HTMLInputElement).addEventListener(
+    "change",
+    () => {
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
+    }
+  );
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  const selectedMode = document.getElementById("mode").value;
+function calculateAndDisplayRoute(
+  directionsService: google.maps.DirectionsService,
+  directionsRenderer: google.maps.DirectionsRenderer
+) {
+  const selectedMode = (document.getElementById("mode") as HTMLInputElement)
+    .value;
 
   directionsService
     .route({
-      origin: { lat: 37.77, lng: -122.447 },
-      destination: { lat: 37.768, lng: -122.511 },
+      origin: { lat: 37.77, lng: -122.447 }, // Haight.
+      destination: { lat: 37.768, lng: -122.511 }, // Ocean Beach.
       // Note that Javascript allows us to access the constant
       // using square brackets and a string value as its
       // "property."
@@ -31,4 +42,9 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     .catch((e) => window.alert("Directions request failed due to " + status));
 }
 
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
 window.initMap = initMap;
