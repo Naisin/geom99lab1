0825cfe69ad0c3cbccfc10fbfeb76548dd53e2be
Naisin <146375897+Naisin@users.noreply.github.com>
2024-01-25 10:01:53 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -2,7 +2,7 @@ const parser = new DOMParser();
 
 async function initMap() {
   // Request needed libraries.
-  const { Map } = await google.maps.importLibrary("maps");
+  const { Map, Polyline } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
     "marker",
   );
@@ -16,7 +16,7 @@ async function initMap() {
   const markerViewWithText = new AdvancedMarkerElement({
     map,
     position: { lat: 33.0308, lng: 74.9490 },
-    title: "Vaishno Devi - The Holy Caves.",
+    title: "Vaishno Devi - The Holy Caves. Best Time: Throughout the year.",
   });
   // Adjust the scale.
   const pinScaled = new PinElement({
@@ -24,9 +24,9 @@ async function initMap() {
   });
   const markerViewScaled = new AdvancedMarkerElement({
     map,
-    position: { lat: 25.3176, lng: 82.9739 },
+    position: { lat: 34.2157, lng: 75.5041 },
     content: pinScaled.element,
-    title: "Varanasi - The Spiritual Capital of India."
+    title: "Amarnath Cave - A haven for pilgrims."
   });
   // Change the background color.
   const pinBackground = new PinElement({
@@ -36,7 +36,7 @@ async function initMap() {
     map,
     position: { lat: 29.9457, lng: 78.1642 },
     content: pinBackground.element,
-    title: "Haridwar - Ganges' wave of religion and spirituality."
+    title: "Haridwar - Ganges' wave of religion and spirituality. Best Time: Throughout the year. "
   });
   // Change the border color.
   const pinBorder = new PinElement({
@@ -44,9 +44,9 @@ async function initMap() {
   });
   const markerViewBorder = new AdvancedMarkerElement({
     map,
-    position: { lat: 34.2157, lng: 75.5041 },
+    position: { lat: 30.9944, lng: 78.9412 },
     content: pinBorder.element,
-    title: "Amarnath Cave - A haven for pilgrims."
+    title: "Gangotri Glacier - Source of River Ganges."
   });
   // Change the glyph color.
   const pinGlyph = new PinElement({
@@ -54,18 +54,40 @@ async function initMap() {
   });
   const markerViewGlyph = new AdvancedMarkerElement({
     map,
-    position: { lat: 24.6959, lng: 84.9914 },
+    position: { lat: 25.3176, lng: 82.9739 },
     content: pinGlyph.element,
-    title: "Bodh Gaya - Mahabodhi Tree & the land of Nirvana."
+    title: "Varanasi - The Spiritual Capital."
   });
   // Hide the glyph.
   const pinNoGlyph = new PinElement({  
   });
   const markerViewNoGlyph = new AdvancedMarkerElement({
     map,
-    position: { lat: 30.9944, lng: 78.9412 },
+    position: { lat: 24.6959, lng: 84.9914 },
     content: pinNoGlyph.element,
-    title: "Gangotri Glacier - Source of River Ganges."
+    title: "Bodh Gaya - Mahabodhi Tree & the land of Nirvana."
   });
+ 
+  const polylineCoordinates = [
+    { lat: 33.0308, lng: 74.9490 },
+    { lat: 34.2157, lng: 75.5041 },
+    { lat: 29.9457, lng: 78.1642 },
+    { lat: 30.9944, lng: 78.9412 },
+    { lat: 25.3176, lng: 82.9739 },
+    { lat: 24.6959, lng: 84.9914 },
+  ];
+
+  // Create a polyline and set its path
+  const flightPath = new Polyline({
+    path: polylineCoordinates,
+    geodesic: true,
+    strokeColor: "red",
+    strokeOpacity: 1.0,
+    strokeWeight: 1,
+  });
+
+  flightPath.setMap(map);
 }
+
 initMap();
+
