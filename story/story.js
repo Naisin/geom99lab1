0c6ff9133fd0d72d364134b1e88c22197ffbe8a3
Naisin <146375897+Naisin@users.noreply.github.com>
2024-01-24 19:53:16 -0500

    Add files via upload

---
+++ story/story.js
@@ -0,0 +1,71 @@
+const parser = new DOMParser();
+
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker",
+  );
+  const map = new Map(document.getElementById("map"), {
+    center: { lat: 32.7266, lng: 74.8570 },
+    zoom: 6,
+    mapId: "4504f8b37365c3d0",
+  });
+  // Each PinElement is paired with a MarkerView to demonstrate setting each parameter.
+  // Default marker with title text (no PinElement).
+  const markerViewWithText = new AdvancedMarkerElement({
+    map,
+    position: { lat: 33.0308, lng: 74.9490 },
+    title: "Vaishno Devi - The Holy Caves. Best Time: Throughout the year.",
+  });
+  // Adjust the scale.
+  const pinScaled = new PinElement({
+
+  });
+  const markerViewScaled = new AdvancedMarkerElement({
+    map,
+    position: { lat: 25.3176, lng: 82.9739 },
+    content: pinScaled.element,
+    title: "Varanasi - The Spiritual Capital of India. Best Time: October to March."
+  });
+  // Change the background color.
+  const pinBackground = new PinElement({
+  
+  });
+  const markerViewBackground = new AdvancedMarkerElement({
+    map,
+    position: { lat: 29.9457, lng: 78.1642 },
+    content: pinBackground.element,
+    title: "Haridwar - Ganges' wave of religion and spirituality. Best Time: Throughout the year. "
+  });
+  // Change the border color.
+  const pinBorder = new PinElement({
+  
+  });
+  const markerViewBorder = new AdvancedMarkerElement({
+    map,
+    position: { lat: 34.2157, lng: 75.5041 },
+    content: pinBorder.element,
+    title: "Amarnath Cave - A haven for pilgrims. Best Time: May to September."
+  });
+  // Change the glyph color.
+  const pinGlyph = new PinElement({
+    
+  });
+  const markerViewGlyph = new AdvancedMarkerElement({
+    map,
+    position: { lat: 24.6959, lng: 84.9914 },
+    content: pinGlyph.element,
+    title: "Bodh Gaya - Mahabodhi Tree & the land of Nirvana. Best Time: October to March."
+  });
+  // Hide the glyph.
+  const pinNoGlyph = new PinElement({  
+  });
+  const markerViewNoGlyph = new AdvancedMarkerElement({
+    map,
+    position: { lat: 30.9944, lng: 78.9412 },
+    content: pinNoGlyph.element,
+    title: "Gangotri Glacier - Source of River Ganges. Best Time: April to June, September to November."
+  });
+}
+initMap();
