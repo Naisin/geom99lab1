fae511f7a45499dc6a1bfb14d4b9583d5bb6ff37
Naisin <146375897+Naisin@users.noreply.github.com>
2024-01-26 12:10:14 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
-    center: { lat: 45.837559, lng: -78.378761 },
+    zoom: 3,
+    center: { lat: -28.024, lng: 140.887 },
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
